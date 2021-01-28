36ec133459fb3754c855e3cdedae33fe61ecfc36
0x6a6f <0x6a6f@users.noreply.github.com>
2021-01-28 12:45:35 +0100

    init

---
+++ index.js
@@ -0,0 +1,119 @@
+const request = require('request');
+
+const getChannelIDbyUser = (username) => {
+  var options = {
+    url: `https://api.twitch.tv/kraken/users?login=${username}`,
+    headers: {
+      'Accept': 'application/vnd.twitchtv.v5+json',
+      'Client-ID': 'kuslq3yf8rm8kcr8rwsmg9aslhhxzi'
+    }
+  };
+  return new Promise((res, rej) => {
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        res(body);
+      } else {
+        rej(error);
+      }
+    });
+  })
+}
+
+const getChannelStatusByID = (user) => {
+  var options = {
+    url: `https://api.twitch.tv/kraken/streams/${user._id}`,
+    headers: {
+      'Accept': 'application/vnd.twitchtv.v5+json',
+      'Client-ID': 'kuslq3yf8rm8kcr8rwsmg9aslhhxzi'
+    }
+  };
+  return new Promise((res, rej) => {
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        res({
+          user,
+          body
+        });
+      } else {
+        rej(error);
+      }
+    });
+  })
+}
+
+const findChannelActivity = (username) => new Promise((resolve, reject) => {
+  getChannelIDbyUser(username)
+    .then(body => {
+      let json = JSON.parse(body);
+      getChannelStatusByID(json.users[0]).then(({
+        user,
+        body
+      }) => {
+        resolve({
+          user,
+          body
+        });
+      })
+    }).catch(error => reject(error));
+});
+
+/*;*/
+
+
+let channels = [];
+let currentData = [];
+
+const getDataForKnownChannels = () => {
+  for (let i = 0; i < channels.length; i++) {
+    findChannelActivity(channels[i].username).then(({
+      user,
+      body
+    }) => {
+      let stream = JSON.parse(body);
+      currentData.push({
+        user,
+        stream,
+        data: channels[i]
+      });
+    })
+  }
+}
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const fs = require('fs');
+
+const getChannelFromFile = () => {
+  let rawdata = fs.readFileSync('channels.json');
+  channels = JSON.parse(rawdata);
+}
+
+const writeNewChannel = (username) => {
+  channels.push(username);
+  let data = JSON.stringify(channels);
+  fs.writeFileSync('channels.json', data);
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.emit('some event', {
+    streams: currentData
+  });
+});
+
+setInterval(() => {
+  io.emit('some event', {
+    streams: currentData
+  });
+}, 5 * 60 * 1000);
+
+http.listen(3000, () => {
+  getChannelFromFile();
+  getDataForKnownChannels();
+  console.log('listening on *:3000');
+});
