bb48d2cc3a230bee44fabaa544de4bd8ccc2efb7
Johannes <holzhauer-johannes@gmx.de>
2021-02-07 18:43:15 +0100

    init

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const cluster = require('cluster');
+if (cluster.isMaster) {
+  cluster.fork();
 
-const app = express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use('/favicon.ico', express.static(path.join(__dirname,'public','images','favicon.ico')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  cluster.on('exit', (worker) => {
+    console.log('Worker ' + worker.id + ' died..');
+    cluster.fork();
+  });
+} else {
+  const express = require('express')
+  const path = require('path')
+  const PORT = process.env.PORT || 5000
+
+  const app = express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .use('/favicon.ico', express.static(path.join(__dirname, 'public', 'images', 'favicon.ico')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
 
 
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-require('./utils/twitch')(io);
+  const http = require('http').createServer(app);
+  const io = require('socket.io')(http);
+  require('./utils/twitch')(io);
 
-http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  process.on('uncaughtException', () => {
+    console.log(err);  
+    process.exit(1);
+  })
+}
