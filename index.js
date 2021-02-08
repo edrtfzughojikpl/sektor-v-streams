3b016be9ecd95363c358b9c74d7b3f50028e66de
Johannes <holzhauer-johannes@gmx.de>
2021-02-08 23:41:42 +0100

    init

--- index.js
+++ index.js
@@ -25,8 +25,12 @@ if (cluster.isMaster) {
 
   http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-  process.on('uncaughtException', () => {
-    console.log(err);  
+  process.on('uncaughtException', (err) => {
+    console.log(`Uncaught Exception: ${err.message}`)
     process.exit(1);
+  })
+  process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled rejection at ', promise, `reason: ${err.message}`)
+    process.exit(1)
   })
 }
