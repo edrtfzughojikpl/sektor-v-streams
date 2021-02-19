8562d159dc6bb34020b1e97a8edd70b0c8f0e78b
Johannes <holzhauer-johannes@gmx.de>
2021-02-19 23:31:38 +0100

    init

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,6 +6,24 @@ const Placeholder = document.querySelector(".wrapper");
 
 let Seconds = 0;
 
+
+/*
+  0 - alphabet
+  1 - fraktion
+  2 - viewer
+  3 - individuell
+*/
+let sortierungsTyp = 0;
+
+/*
+  0 - online
+  1 - offline
+  2 - online&offline
+  3 - nichts(?)
+*/
+let einblendungsTyp = 0;
+
+
 let timer = setInterval(() => {
   updateTimer(Seconds);
   Seconds++;
@@ -20,14 +38,32 @@ const updateTimer = (seconds) => {
   timeElement.textContent = `${(minutes<10)?"0"+minutes:minutes}:${(seconds<10)?"0"+seconds:seconds}`;
 };
 
+let _channels;
+
 socket.on('data', ({
   channels,
   lastUpdated
 }) => {
   console.log(channels, lastUpdated);
 
+  _channels = channels;
+
   let passedSeconds = calculate(lastUpdated);
   Seconds = passedSeconds;
+
+  switch (sortierungsTyp) {
+    case 1:
+      channels.sort((a, b) => a.charInfo.organisation - b.charInfo.organisation);
+      break;
+    case 2:
+      channels.sort((a, b) => a.stream.viewer - b.stream.viewer);
+      break;
+    case 3:
+      //
+      break;
+    default:
+      break;
+  }
 
   channels.forEach(channel => {
     let card = document.querySelector(`[data-name="${channel.user.display_name}"]`);
@@ -85,7 +121,7 @@ const createNewCard = ({
   orga.textContent = charInfo.organisation;
   charRow.append(orga);
 
-  if(charInfo.otherChars){
+  if (charInfo.otherChars) {
     let icon = element.querySelector(".icon");
     icon.style.display = "block";
     charInfo.otherChars.forEach((item) => {
@@ -112,7 +148,82 @@ function calculate(lastUpdated) {
   return diffSeconds;
 }
 
+var elements = document.getElementsByClassName("container");
 
+var myFunction = function () {
+  switch (this.parentElement.textContent.replace(/\s/g, '')) {
+    case "Fraktion":
+      sortierungsTyp = 1;
+      break;
+    case "Viewer":
+      sortierungsTyp = 2;
+      break;
+    case "Individuell":
+      sortierungsTyp = 3;
+      break;
+    default: //Alphabetisch
+      sortierungsTyp = 0;
+      break;
+  }
+
+  switch (sortierungsTyp) {
+    case 1:
+      _channels = _channels.sort((a, b) => a.charInfo.organisation.localeCompare(b.charInfo.organisation));
+      break;
+    case 2:
+      let __channels = _channels;
+      _channels = [];
+      __channels.forEach((channel) => {
+        if (channel.stream) _channels.push(channel);
+      });
+      _channels = _channels.sort((a, b) => {
+        return Number(b.stream.viewers) - Number(a.stream.viewers);
+      });
+      break;
+    case 3:
+      //
+      break;
+    default:
+      _channels = _channels.sort((a, b) => a.user.display_name.localeCompare(b.user.display_name));
+      break;
+  }
+  
+  rootElement.innerHTML = "";
+
+  _channels.forEach(channel => {
+    let card = document.querySelector(`[data-name="${channel.user.display_name}"]`);
+    if (!card) {
+      if (channel.stream) {
+        if (channel.stream.game == 'Grand Theft Auto V') {
+          createNewCard(channel);
+        }
+      }
+    } else
+    if (!channel.stream) {
+      console.log(`${card.dataset.name} went offline!`, new Date().toLocaleString());
+      card.parentElement.parentElement.remove();
+    } else {
+      if (channel.stream.game != 'Grand Theft Auto V') {
+        console.log(`${card.dataset.name} went offline!`, new Date().toLocaleString());
+        card.parentElement.parentElement.remove();
+      }
+    }
+  })
+};
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].firstElementChild.addEventListener('click', myFunction, false);
+}
+
+var elements0 = document.getElementsByClassName("container0");
+
+var myFunction0 = function () {
+  console.log(this.parentElement.textContent.replace(/\s/g, ''));
+};
+
+for (var i = 0; i < elements0.length; i++) {
+  elements0[i].firstElementChild.addEventListener('click', myFunction0, false);
+}
 
 
 // { DISABLED! (only plays on HS)
